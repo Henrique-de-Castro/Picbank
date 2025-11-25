0c5b5c32780fc88894fc2a9b1671bea17d303aca
e-shigueru <enzo.shigueru@picpay.com>
2025-11-25 17:32:36 -0300

    paginar component e trocando templete do home

--- front/src/app/components/paginar/paginar.component.ts
+++ front/src/app/components/paginar/paginar.component.ts
@@ -6,29 +6,20 @@ import { PageEvent } from '@angular/material/paginator';
   templateUrl: './paginar.component.html',
   styleUrls: ['./paginar.component.scss']
 })
+
 export class PaginarComponent implements OnInit {
 
-  // total de registros que vêm do backend
   totalItems: number = 100;
-
-  // atual page size
   itemsPerPage: number = 5;
-
-  // opções de quantidade por página
   opItemsPerPage: number[] = [5, 10, 20];
 
-  // manda número da página atual
   @Output() page = new EventEmitter<number>();
-
-  // manda quantidade por página
   @Output() perPage = new EventEmitter<number>();
 
-  ngOnInit(): void {
-    // valores padrão já passados, se quiser pode receber via @Input()
-  }
+  ngOnInit(): void {}
 
   onPageChange(event: PageEvent): void {
-    this.page.emit(event.pageIndex + 1); // pagina atual (base 1)
-    this.perPage.emit(event.pageSize);   // quantidade por página selecionada
+    this.page.emit(event.pageIndex + 1); // base 1
+    this.perPage.emit(event.pageSize);
   }
 }
